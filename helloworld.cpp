fe490077657bc98864698688fb1461b1e2aebd30
Brijeshvekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-06-28 17:38:44 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello world");
+	printf("Addition of 250 and 125 is %d\n", 250+125);
+	printf("Subtraction of 250 and 125 is %d\n", 250-125);
+	printf("Multiplication of 250 and 125 is %d\n", 250*125);
+	printf("Division of 250 and 125 is %d\n", 250/125);
+	printf("Remainder of 250 and 125 is %d\n", 250%125);
+	
+}
