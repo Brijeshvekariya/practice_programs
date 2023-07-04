9d2a0db946f96add7f65c76fd72f4edbaacc1f0d
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-04 17:29:06 +0530

    Add files via upload

---
+++ mobilestore.c
@@ -0,0 +1,292 @@
+#include<stdio.h>
+
+int main()
+{
+	int brand,model,buy,series;
+	printf("------------Welcome to ROYAL Mobiles------------\n");
+	printf("Please choose the brand from below options :\n");
+	printf("1. Apple \n2. Samsung \n3. One plus \n");
+	printf("Enter your choice :- ");
+	scanf("%d",&brand);
+	if(brand==1)
+	{
+		
+		printf("\n\n");
+		printf("Please select series from below options : \n");
+		printf("1. Iphone 14 series \n2. Iphone 13 series \n3.Iphone 12 series \n");
+		printf("Enter your choice :- ");
+		scanf("%d",&series);
+		if(series==1)
+		{
+		
+			printf("\n\n");
+			printf("Please select model from below options : \n");
+			printf("1. Iphone 14 \n2. Iphone 14 pro \n3.Iphone 14 pro maxx \n");
+			printf("Enter your choice :- ");
+			scanf("%d",&model);
+			if(model==1)
+			{
+				printf("\n\n");
+				printf("Price of Iphone 14 is Rs. 92,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else if(model==2)
+			{
+				printf("\n\n");
+				printf("Price of Iphone 14 pro is Rs. 1,12,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else
+			{
+				printf("\n\n");
+				printf("Price of Iphone 14 pro maxx is Rs. 1,42,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+		}
+		else if(series==2)
+		{
+			printf("\n\n");
+			printf("Please select model from below options : \n");
+			printf("1. Iphone 13 \n2. Iphone 13 pro \n3.Iphone 13 pro maxx \n");
+			printf("Enter your choice :- ");
+			scanf("%d",&model);
+			if(model==1)
+			{
+				printf("\n\n");
+				printf("Price of Iphone 13 is Rs. 82,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else if(model==2)
+			{
+				printf("\n\n");
+				printf("Price of Iphone 13 pro is Rs. 1,02,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else
+			{
+				printf("\n\n");
+				printf("Price of Iphone 13 pro maxx is Rs. 1,32,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			
+		}
+		else
+		{
+			printf("\n\n");
+			printf("Please select model from below options : \n");
+			printf("1. Iphone 12 \n2. Iphone 12 pro \n3.Iphone 12 pro maxx \n");
+			printf("Enter your choice :- ");
+			scanf("%d",&model);
+			if(model==1)
+			{
+				printf("\n\n");
+				printf("Price of Iphone 12 is Rs. 62,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else if(model==2)
+			{
+				printf("\n\n");
+				printf("Price of Iphone 12 pro is Rs. 82,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else
+			{
+				printf("\n\n");
+				printf("Price of Iphone 12 pro maxx is Rs. 1,02,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			
+		}
+		
+		
+	}
+	else if(brand=2)
+	{
+		
+			printf("\n\n");
+			printf("Please select model from below options : \n");
+			printf("1. Samsung S23 \n2. Samsung S22 \n3.Samsung S21 \n");
+			printf("Enter your choice :- ");
+			scanf("%d",&model);
+			if(model==1)
+			{
+				printf("\n\n");
+				printf("Price of Samsung S23 is Rs. 85,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else if(model==2)
+			{
+				printf("\n\n");
+				printf("Price of Samsung S22 is Rs. 77,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else
+			{
+				printf("\n\n");
+				printf("Price of Samsung S21 is Rs. 52,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+	}
+	else
+	{
+			printf("\n\n");
+			printf("Please select model from below options : \n");
+			printf("1. Oneplus nord 11 \n2. One plus nord 10 \n3. One plus nord 8 \n");
+			printf("Enter your choice :- ");
+			scanf("%d",&model);
+			if(model==1)
+			{
+				printf("\n\n");
+				printf("Price of One plus nord 11 is Rs. 45,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else if(model==2)
+			{
+				printf("\n\n");
+				printf("Price of One plus Nord 10 is Rs. 37,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+			else
+			{
+				printf("\n\n");
+				printf("Price of One plus Nord 8 is Rs. 27,000/- + gst 18%% \n");
+				printf("Please select :\n1 for buy option \n2 for cancel option \n");
+				scanf("%d",&buy);
+				if(buy==1)
+				{
+					printf("Successfully product added to cart :)");
+				}
+				else
+				{
+					printf("quit to main page");
+				}
+			}
+		
+	}
+}
