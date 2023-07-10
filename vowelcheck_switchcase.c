8d61f38b7441ed98beab907f7a66fee8652b095f
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-10 14:07:53 +0530

    Add files via upload

---
+++ vowelcheck_switchcase.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int main()
+{
+	char alphabet;
+	printf("Enter any Alphabet :\n");
+	scanf("%c",&alphabet);
+	switch(alphabet)
+	{
+		case 'A':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'E':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'I':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'O':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'U':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'a':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'e':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'i':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'o':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		case 'u':
+			{
+				printf("%c character is Vowel \n",alphabet);
+				break;
+			}
+		default :
+			{
+				printf("%c character is constant \n",alphabet);
+			}
+	}
+}
