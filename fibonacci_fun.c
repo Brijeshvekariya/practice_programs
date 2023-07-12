0efe2ca77076ceed25811a649889e054dfea10fd
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-12 17:50:40 +0530

    Add files via upload

---
+++ fibonacci_fun.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+	int t1=0,t2=1,nextterm=t1+t2;
+	for(int i=3;i<=n;i++)
+	{
+		printf("%d ",nextterm);
+		t1 = t2;
+		t2 = nextterm;
+		nextterm = t1+t2;
+	}
+	
+}
+int main()
+{
+	int i,n;
+	int t1=0,t2=1;
+	int nextterm = t1+t2;
+	printf("Enter the number of terms : ");
+	scanf("%d",&n);
+	printf("%d %d ",t1,t2);
+	fibonacci(n);
+	
+	
+}
