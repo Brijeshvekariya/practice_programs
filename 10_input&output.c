ce373143e7852f87f0468785e8f749b0dc331d27
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-15 22:48:05 +0530

    Add files via upload

---
+++ 10_input&output.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,input[10];
+	printf("Enter any 10 numbers :\n");
+	for(i=0; i<10;i++)
+	{
+		printf(" %d  number : ",i+1);
+		scanf(" %d",&input[i]);
+	}
+	for(i=0; i<10;i++)
+	{
+		printf("Entered %d number is : %d\n",i+1,input[i]); 
+ 	}
+ }
