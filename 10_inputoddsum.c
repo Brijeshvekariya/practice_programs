06185d5760ff0b6c2069a92e089fa1fe59e3c00b
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-16 17:25:45 +0530

    Add files via upload

---
+++ 10_inputoddsum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,input[10],sum=0;
+	printf("Enter any 10 numbers :\n");
+	for(i=0; i<10;i++)
+	{
+		printf(" %d  number : ",i+1);
+		scanf(" %d",&input[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		if(input[i]%2!=0)
+		{
+			printf("%d ",input[i]);
+			sum+=input[i];
+		} 
+	}
+	printf("are the odd numbers from given input.\n");
+	printf("The Sum of all odd number is : %d",sum);
+}
