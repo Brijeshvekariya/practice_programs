58d86ca839e09cc5085235c866ae70912733e942
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-16 17:25:27 +0530

    Add files via upload

---
+++ 10_inputodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,input[10];
+	printf("Enter any 10 numbers :\n");
+	for(i=0; i<10;i++)
+	{
+		printf(" %d  number : ",i+1);
+		scanf(" %d",&input[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		if(input[i]%2!=0)
+		{
+			printf("\n%d ",input[i]);
+		}
+	}
+	printf("are the odd numbers from given input.");
+}
