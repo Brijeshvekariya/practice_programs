839c03c909c8185d696bcda631c670172e9113ce
Brijesh Vekariya <137178081+Brijeshvekariya@users.noreply.github.com>
2023-07-22 23:18:11 +0530

    Add files via upload

---
+++ B-star.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+	for(int i = 0;i<=15;i++)
+	{
+		printf("*");
+	}
+	for(int i = 0;i<7;i++)
+	{
+		printf("\n*");
+		for(int j = 14;j>0;j--)
+		{
+			printf(" ");
+		}
+		printf("*");
+	}
+	printf("\n");
+	for(int i = 0;i<=15;i++)
+	{
+		printf("*");
+	}
+	for(int i = 0;i<7;i++)
+	{
+		printf("\n*");
+		for(int j = 14;j>0;j--)
+		{
+			printf(" ");
+		}
+		printf("*");
+	}
+	printf("\n");
+	for(int i = 0;i<=15;i++)
+	{
+		printf("*");
+	}
+	printf("\n\n\n\n\n\n");
+}
